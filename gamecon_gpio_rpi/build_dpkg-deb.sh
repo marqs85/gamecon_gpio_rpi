315388cbd3a3a63385e30e9a451fd059e5ab9a44
marqs <mhiienka@niksula.hut.fi>
2024-10-09 23:04:54 +0300

    tweak build script to produce .deb like earlier

--- gamecon_gpio_rpi/build_dpkg-deb.sh
+++ gamecon_gpio_rpi/build_dpkg-deb.sh
@@ -5,19 +5,21 @@ source dkms.conf
 DEB_NAME=$(echo $PACKAGE_NAME | sed 's,_,-,g')
 
 MKDEB_DIR=${PACKAGE_NAME}-dkms-mkdeb
-BUILD_DIR=${DEB_NAME}_${PACKAGE_VERSION}
+BUILD_DIR=${DEB_NAME}-dkms_${PACKAGE_VERSION}_all
 
 mkdir -p $BUILD_DIR/DEBIAN
 sed -e "s/DEBIAN_PACKAGE/${DEB_NAME}/g" -e '/^$/d' $MKDEB_DIR/debian/control > $BUILD_DIR/DEBIAN/control
 echo "Version: ${PACKAGE_VERSION}" >> $BUILD_DIR/DEBIAN/control
-sed -e "s/NAME=MODULE_NAME/NAME=${DEB_NAME}/g" $MKDEB_DIR/debian/postinst > $BUILD_DIR/DEBIAN/postinst
-sed -e "s/NAME=MODULE_NAME/NAME=${DEB_NAME}/g" -e "s/VERSION=MODULE_VERSION/VERSION=${PACKAGE_VERSION}/g" $MKDEB_DIR/debian/prerm > $BUILD_DIR/DEBIAN/prerm
+sed -e "s/DEBIAN_PACKAGE/${PACKAGE_NAME}/g" -e "s/MODULE_VERSION/${PACKAGE_VERSION}/g" -e "s/DATE_STAMP/$(date)/g" $MKDEB_DIR/debian/changelog > $BUILD_DIR/DEBIAN/changelog
+sed -e "s/NAME=MODULE_NAME/NAME=${PACKAGE_NAME}/g" $MKDEB_DIR/debian/postinst > $BUILD_DIR/DEBIAN/postinst
+sed -e "s/NAME=MODULE_NAME/NAME=${PACKAGE_NAME}/g" -e "s/VERSION=MODULE_VERSION/VERSION=${PACKAGE_VERSION}/g" $MKDEB_DIR/debian/prerm > $BUILD_DIR/DEBIAN/prerm
 chmod 775 $BUILD_DIR/DEBIAN/postinst $BUILD_DIR/DEBIAN/prerm
 
-mkdir -p $BUILD_DIR/usr/share/doc
-cp README $BUILD_DIR/usr/share/doc
+mkdir -p $BUILD_DIR/usr/share/doc/$PACKAGE_NAME
+cp README $BUILD_DIR/usr/share/doc/$PACKAGE_NAME
+gzip $BUILD_DIR/usr/share/doc/$PACKAGE_NAME/README
 
-mkdir -p $BUILD_DIR/usr/src/${DEB_NAME}-${PACKAGE_VERSION}
-cp -t $BUILD_DIR/usr/src/${DEB_NAME}-${PACKAGE_VERSION} dkms.conf ${PACKAGE_NAME}.c Makefile
+mkdir -p $BUILD_DIR/usr/src/${PACKAGE_NAME}-${PACKAGE_VERSION}
+cp -t $BUILD_DIR/usr/src/${PACKAGE_NAME}-${PACKAGE_VERSION} dkms.conf ${PACKAGE_NAME}.c Makefile
 
 dpkg-deb --build $BUILD_DIR
